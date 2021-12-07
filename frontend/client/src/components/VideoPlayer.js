001ded48a97d8f18af60cc52085b90f966a83a42
tejaswani23 <tejaswaniagrawal23@gmail.com>
2021-12-08 03:04:20 +0530

    [Frontend] videoplayer modified

--- frontend/client/src/components/VideoPlayer.js
+++ frontend/client/src/components/VideoPlayer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import {Grid,Typography,Paper} from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import { SocketContext } from '../SocketContext';
@@ -24,22 +24,31 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 const VideoPlayer = () => {
-
+  const {name,callAccepted,myVideo,userVideo,callEnded,stream,call}= useContext(SocketContext);
     const classes=useStyles();
     return (
        <Grid container className={classes.gridContainer}>
-           <Paper className={classes.paper}>
+       {
+           stream && (
+            <Paper className={classes.paper}>
                <Grid item xs={12} md={6}>
-<Typography variant="h5" gutterBottom>Name</Typography>
-<video playsInline muted ref={null} autoPlay className={classes.video}/>
+<Typography variant="h5" gutterBottom>{name||'Name'}</Typography>
+<video playsInline muted ref={myVideo} autoPlay className={classes.video}/>
                </Grid>
            </Paper>
-           <Paper className={classes.paper}>
+           )
+       }
+         {
+             callAccepted && !callEnded && (
+                <Paper className={classes.paper}>
                <Grid item xs={12} md={6}>
-<Typography variant="h5" gutterBottom>Name</Typography>
-<video playsInline  ref={null} autoPlay className={classes.video}/>
+<Typography variant="h5" gutterBottom>{call.name||'Name'}</Typography>
+<video playsInline  ref={userVideo} autoPlay className={classes.video}/>
                </Grid>
            </Paper>
+             )
+         }
+          
        </Grid>
     )
 }
