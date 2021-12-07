e15e3de3c1aa0d48aa08aee41ceb40a96ed78858
tejaswani23 <tejaswaniagrawal23@gmail.com>
2021-12-08 02:55:41 +0530

    socket connected

---
+++ frontend/client/src/components/Notifications.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Notifications = () => {
+    return (
+        <div>
+            Notifications
+        </div>
+    )
+}
+
+
+export default Notifications;
--- frontend/client/src/index.js
+++ frontend/client/src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import "./styles.css";
+import { ContextProvider } from './SocketContext';
 import App from './App';
 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ContextProvider>
+<App />
+  </ContextProvider>
+    
+  ,
   document.getElementById('root')
 );
 
